b6de187413d75f2e7dc454d2b7321d37392321d0
maryamalizadeh91 <maryamalizadeh91@gmail.com>
2018-12-26 10:40:56 +0330

    LinkedList Definition

---
+++ linkedList.js
@@ -0,0 +1,19 @@
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+}
+
+function Node(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+}
+
+var LL = new LinkedList();
+var userList = new LinkedList();
+var toDoList = new LinkedList();
+
+var node = new Node(100, 'node2', null);
+
+console.log(node);
+console.log(LL);
