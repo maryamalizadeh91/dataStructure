6b035d59e235b8eaca348e684abc38ee671439c3
maryamalizadeh91 <maryamalizadeh91@gmail.com>
2018-12-26 10:45:56 +0330

    Add to head part

--- linkedList.js
+++ linkedList.js
@@ -4,16 +4,20 @@ function LinkedList() {
 }
 
 function Node(value, next, prev) {
-    this.value = value;
-    this.next = next;
-    this.prev = prev;
+  this.value = value;
+  this.next = next;
+  this.prev = prev;
 }
 
-var LL = new LinkedList();
+/*var LL = new LinkedList();
 var userList = new LinkedList();
 var toDoList = new LinkedList();
 
 var node = new Node(100, 'node2', null);
 
 console.log(node);
-console.log(LL);
+console.log(LL);*/
+
+LinkedList.prototype.addToHead = function(value) {
+  var newNode = Node(value, this.head, null);
+};
