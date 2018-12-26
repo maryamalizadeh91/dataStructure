cdb2971d53dd887cff939bef1d764ddccc684b17
maryamalizadeh91 <maryamalizadeh91@gmail.com>
2018-12-26 09:52:51 +0330

    Constructor Function

---
+++ constructorFunction.js
@@ -0,0 +1,8 @@
+function User(firstName, lastName, age, gender) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.gender = gender;
+  this.age = age;
+}
+
+var user1 = new User("Maryam", "Alizadeh", 27, "female");
