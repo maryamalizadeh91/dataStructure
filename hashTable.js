f51428ecdb649b2392031bf26feca8759083ba15
maryamalizadeh91 <maryamalizadeh91@gmail.com>
2018-12-27 11:34:17 +0330

    HashTable hash  function

--- hashTable.js
+++ hashTable.js
@@ -1,14 +1,23 @@
 function HashTable(size) {
-    this.buckets = Array(size);
-    this.numBuckets = this.buckets.length;
+  this.buckets = Array(size);
+  this.numBuckets = this.buckets.length;
 }
 
 function HashNode(key, value, next) {
-    this.key = key;
-    this.value = value;
-    this.next = next || null;
+  this.key = key;
+  this.value = value;
+  this.next = next || null;
 }
+
+HashTable.prototype.hash = function(key) {
+  total = 0;
+  for (i = 0; i < key.length; i++) {
+    total += key.charCodeAt(i);
+  }
+  var bucket = total % this.numBuckets;
+  return bucket;
+};
 
 var myHT = new HashTable(30);
 
-console.log(myHT);
+console.log(myHT.hash('Becca'));
